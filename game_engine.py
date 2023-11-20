ecc1ce06722ae08aef65f039ab6323ea5bc8a1c3
saltchicken <john.eicher89@gmail.com>
2023-11-20 12:22:54 -0800

    Refactored event_loop to game_engine

--- game_engine.py
+++ game_engine.py
@@ -13,8 +13,12 @@ class GameEngine:
     def __init__(self) -> None:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Chess Board")
+        self.running = True
         self.images = {}
         self.load_pieces()
+        self.selected_piece = None
+        self.offset_x = 0
+        self.offset_y = 0
         
     def load_pieces(self) -> None:
         pieces = ["b_rook", "w_rook"]
@@ -34,4 +38,24 @@ class GameEngine:
     def draw_pieces(self):
         for piece_obj in self.pieces.values():
             piece_obj.draw(self.screen)
+            
+    def event_loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                for piece, piece_obj in self.pieces.items():
+                    if piece_obj.rect.collidepoint(event.pos):
+                        self.selected_piece = piece_obj
+                        self.offset_x = piece_obj.position[0] - event.pos[0]
+                        self.offset_y = piece_obj.position[1] - event.pos[1]
+                        break
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if self.selected_piece:
+                    self.selected_piece.snap_to_grid((self.offset_x, self.offset_y))
+                    self.selected_piece = None
+            elif event.type == pygame.MOUSEMOTION:
+                if self.selected_piece:
+                    mouse_x, mouse_y = event.pos
+                    self.selected_piece.move((mouse_x + self.offset_x, mouse_y + self.offset_y))
             
--- main.py
+++ main.py
@@ -6,31 +6,8 @@ pygame.init()
 def main():
     clock = pygame.time.Clock()
     game_engine = GameEngine()
-
-    selected_piece = None
-    offset_x = offset_y = 0
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                for piece, piece_obj in game_engine.pieces.items():
-                    if piece_obj.rect.collidepoint(event.pos):
-                        selected_piece = piece_obj
-                        offset_x = piece_obj.position[0] - event.pos[0]
-                        offset_y = piece_obj.position[1] - event.pos[1]
-                        break
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if selected_piece:
-                    selected_piece.snap_to_grid((offset_x, offset_y))
-                    selected_piece = None
-            elif event.type == pygame.MOUSEMOTION:
-                if selected_piece:
-                    mouse_x, mouse_y = event.pos
-                    selected_piece.move((mouse_x + offset_x, mouse_y + offset_y))
-
+    while game_engine.running:
+        game_engine.event_loop()
         game_engine.draw_board()
         game_engine.draw_pieces()
         pygame.display.flip()
